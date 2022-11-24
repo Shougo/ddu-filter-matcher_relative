26d7b5d4721f6bc3c91f0e524cec2a0d567da7b8
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-11-24 19:33:56 +0900

    Add override

--- denops/@ddu-filters/matcher_relative.ts
+++ denops/@ddu-filters/matcher_relative.ts
@@ -1,12 +1,16 @@
-import { BaseFilter, DduItem, SourceOptions } from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+import {
+  BaseFilter,
+  DduItem,
+  SourceOptions,
+} from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.1/file.ts#^";
-import { relative } from "https://deno.land/std@0.165.0/path/mod.ts#^";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.1/file.ts";
+import { relative } from "https://deno.land/std@0.165.0/path/mod.ts";
 
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
-  async filter(args: {
+  override async filter(args: {
     denops: Denops;
     sourceOptions: SourceOptions;
     input: string;
@@ -16,14 +20,14 @@ export class Filter extends BaseFilter<Params> {
     return Promise.resolve(args.items.filter(
       (item) => {
         const action = item.action as ActionData;
-        if (!action.path) { return false; }
+        if (!action.path) return false;
         const relativePath = relative(cwd, action.path);
         return relativePath != action.path && !relativePath.startsWith("..");
-      }
+      },
     ));
   }
 
-  params(): Params {
+  override params(): Params {
     return {};
   }
 }
