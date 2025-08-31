f0b00d2e35be2afba0b0644c593de7098b431f5f
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-08-31 15:52:58 +0900

    Use import-map

--- denops/@ddu-filters/matcher_relative/main.ts
+++ denops/@ddu-filters/matcher_relative/main.ts
@@ -1,16 +1,13 @@
-import {
-  type DduItem,
-  type SourceOptions,
-} from "jsr:@shougo/ddu-vim@~6.1.0/types";
-import { BaseFilter } from "jsr:@shougo/ddu-vim@~6.1.0/filter";
-import { treePath2Filename } from "jsr:@shougo/ddu-vim@~6.1.0/utils";
+import type { DduItem, SourceOptions } from "@shougo/ddu-vim/types";
+import { BaseFilter } from "@shougo/ddu-vim/filter";
+import { treePath2Filename } from "@shougo/ddu-vim/utils";
 
-import { type ActionData } from "jsr:@shougo/ddu-kind-file@~0.9.0";
+import type { ActionData } from "@shougo/ddu-kind-file";
 
-import type { Denops } from "jsr:@denops/core@~7.0.0";
-import * as fn from "jsr:@denops/std@~7.1.1/function";
+import type { Denops } from "@denops/std";
+import * as fn from "@denops/std/function";
 
-import { relative } from "jsr:@std/path@~1.0.3/relative";
+import { relative } from "@std/path/relative";
 
 type Params = Record<string, never>;
 
